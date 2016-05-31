ccd299801673b30354162c0948d5250f575944e7
Russell Bruce Davidson <RBD@Russells-MacBook-Pro.local>
2016-05-30 21:25:30 -0600

    finished the covar array analysis part of this script; add a PCA analysis at the end?

--- PCA_cartesian.py
+++ PCA_cartesian.py
@@ -41,7 +41,98 @@ def ffprint(string):
 # ----------------------------------------
 # MAIN PROGRAM:
 
+# INITIATE AND CALCULATE THE IMPORTANT INFORMATION ABOUT THE AVG STRUCTURE
 avg = MDAnalysis.Universe(avg_pdb)
+avg_all = avg.select_atoms('all')
+avg_backbone = avg.select_atoms('backbone')
+avg_align = avg.select_atoms(alignment)
+avg_important = avg.select_atoms(important)
+
+avg_all.translate(-avg_backbone.center_of_mass())
+pos0 = avg_align.positions
+
+nRes = len(avg_important.residues)
+avgCoord = zeros((nRes,3),dtype=float)
+
+for i in range(nRes):
+	res0 = avg_important.residues[i]
+	com0 = res0.center_of_mass()
+	avgCoord[i,:] = com0
+
+# INITIATE AND CREATE THE IMPORTANT ATOM SELECTIONS FOR THE IMPORTANT UNIVERSE
+u = MDAnalaysis.Universe(pdb_file)
+u_all = u.select_atoms('all')
+u_backbone = u.select_atoms('backbone')
+u_align = u.select_atoms(alignment)
+u_important = u.select_atoms(important)
+
+if nRes != len(u_important.residues):
+	ffprint('Number of residues do not match between average structure and trajectory universes')
+	sys.exit()
+
+# COLLECTING NSTEPS DATA... NEED TO ITERATE THROUGH ALL TRAJECTORIES TO COLLECT THIS...
+nSteps = 0
+temp = start
+while temp <= end:
+	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,temp,temp))
+	nSteps += len(u.trajectory)
+	temp += 1
+
+# ARRAY DECLARATION
+allCoord = zeros((nSteps,nRes,3),dtype=float)
+covar_array = zeros((3*nRes, 3*nRes),dtype=float)
+temp_array = zeros(3*nRes,dtype=float)
+
+# TRAJECTORY ANALYSIS; COLLECTING THE COM INFO OF EACH RESIDUE OF INTEREST
+temp = 0
+while start <= end:
+	ffprint('Loading trajectory %s' %(start))
+	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,start,start))
+
+	for ts in u.trajectory:
+		dimensions = u.dimensions[:3]
+		u_all.translate(-u_backbone.center_of_mass())
+
+		R,d = rotation_matrix(u_align.positions,pos0)
+		u_all.rotate(R)
+
+		for i in range(nRes):
+			res0 = u_important.residues[i]
+			com0 = res0.center_of_mass()
+			allCoord[temp,i,:] = com0
+
+		temp += 1 
+	start += 1
+
+# CALCULATING THE COVAR ARRAY OF COM CARTESIAN COORDINATES
+for i in range(nSteps):
+	temp_array = flatten(allCoord[i])
+	for res1 in range(3*nRes):
+		for res2 in range(res1,3*nRes):
+			covar_array[res1,res2] += temp_array[res1]*temp_array[res2]
+
+covar_array /= nSteps
+
+# COMPLETE THE COVAR MATRIX ANALYSIS; NEED TO SUBTRACT OUT THE MEAN
+temp_array = flatten(avgCoord)
+for res1 in range(3*nRes):
+	for res2 in range(res1,3*nRes):
+		covar_array[res1,res2] -= temp_array[res1]*temp_array[res2]
+		covar_array[res2,res1] = covar_array[res1,res2]
+
+# OUTPUTING THE COVAR MATRIX
+out1 = open('%03d.%03d.cover_matrix.dat' %(sys.argv[3],end),'w')
+for i in range(3*nRes):
+	for j in range(3*nRes):
+		out1.write('%e   ' %(covar_array[i,j]))
+	out1.write('\n')
+out1.close()
+
+# WRITE A PCA ANALYSIS OF THIS COVAR ARRAY? 
+
+
+
+
 
 
 
