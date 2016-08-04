134c521b84688a4bcb2b1f09db30a377c8e121be
Russell Bruce Davidson <RBD@Russells-MacBook-Pro.local>
2016-08-03 23:19:09 -0600

    PCA analysis of the cartesian covar matrix is producing eigenvalues/vectors that have complex components; not sure where this is coming from; so still more debugging to be done before the PCA analysis will be useful

--- test_suite3/Covar_calc.py
+++ test_suite3/Covar_calc.py
@@ -25,6 +25,7 @@ traj_loc = sys.argv[2]
 start = int(sys.argv[3])
 end = int(sys.argv[4])
 avg_pdb = sys.argv[5]
+avg_dcd = sys.argv[6]
 
 alignment = 'protein and name CA and (resid 20:25 or resid 50:55 or resid 73:75 or resid 90:94 or resid 112:116 or resid 142:147 or resid 165:169 or resid 190:194 or resid 214:218 or resid 236:240 or resid 253:258 or resid 303:307)'
 important = 'protein'
@@ -52,19 +53,15 @@ def ffprint(string):
 # MAIN PROGRAM:
 # ----------------------------------------
 # INITIATE AND CALCULATE THE IMPORTANT INFORMATION ABOUT THE AVG STRUCTURE
-avg = MDAnalysis.Universe(avg_pdb)
+avg = MDAnalysis.Universe(avg_pdb,avg_dcd)
+#avg = MDAnalysis.Universe(avg_pdb)
 avg_all = avg.select_atoms('all')
 avg_align = avg.select_atoms(alignment)
 avg_important = avg.select_atoms(important)
 
 avg_all.translate(-avg_align.center_of_mass())
 pos0 = avg_align.positions
-
 nRes = avg_important.n_residues
-avgCoord = zeros((nRes,3),dtype=np.float64)
-
-for i in range(nRes):
-	avgCoord[i,:] = avg_important.residues[i].center_of_mass()
 
 # ----------------------------------------
 # INITIATE AND CREATE THE IMPORTANT ATOM SELECTIONS FOR THE IMPORTANT UNIVERSE
@@ -87,10 +84,8 @@ while temp <= end:
 
 # ARRAY DECLARATION
 allCoord = zeros((nSteps,nRes,3),dtype=np.float64)
-cart_covar_array = zeros((3*nRes, 3*nRes),dtype=np.float64)
+avgCoord = zeros((nRes,3),dtype=np.float64)
 dist_covar_array = zeros((nRes,nRes),dtype=np.float64)
-test_array = zeros((nRes,nRes),dtype=np.float64)
-temp_array = zeros(3*nRes,dtype=np.float64)
 msd_array = zeros(nRes,dtype=np.float64)
 
 # TRAJECTORY ANALYSIS; COLLECTING THE COM INFO OF EACH RESIDUE OF INTEREST
@@ -104,35 +99,35 @@ while start <= end:
 		u_all.rotate(R)
 		for i in range(nRes):
 			allCoord[ts.frame,i,:] = u_important.residues[i].center_of_mass()
+			avgCoord[i,:] += u_important.residues[i].center_of_mass()
 	start += 1
+
+avgCoord /= nSteps
 
 # ----------------------------------------
 # CALCULATING THE DISTANCE COVAR MATRIX OF RESIDUE RESIDUE PAIRS
 for i in range(nSteps):
 	for res1 in range(nRes):
-		delta_r = allCoord[i,res1,:] - avgCoord[res1,:]		# Calculating the delta r for every timestep
+		delta_r = allCoord[i,res1,:]-avgCoord[res1,:]		# Calculating the delta r for every timestep
 		msd_array[res1] += dot_prod(delta_r,delta_r)		# Sum over all timesteps; 
 		for res2 in range(res1,nRes):
-			temp = allCoord[i,res2,:] - avgCoord[res2,:]
-			test_array += dot_prod(delta_r,temp)
-			dist_covar_array[res1,res2] += dot_prod(allCoord[i,res1,:],allCoord[i,res2,:])	# Sum over all timesteps;
+			temp = allCoord[i,res2,:]-avgCoord[res2,:]
+			dist_covar_array[res1,res2] += dot_prod(delta_r,temp)
+			
 dist_covar_array /= nSteps
-test_array /= nSteps
 msd_array /= nSteps
 
 # COMPLETE THE DISTANCE COVAR MATRIX ANALYSIS BY SUBTRACTING OUT THE MEAN AND NORMALIZING BY THE VARIANCE
 for res1 in range(nRes):
 	for res2 in range(res1,nRes):
-		dist_covar_array[res1,res2] -= dot_prod(avgCoord[res1,:],avgCoord[res2,:])	# Subtracting out the mean positions
-#		dist_covar_array[res1,res2] /= sqrt(msd_array[res1]*msd_array[res2])	# Normalizing each matrix element by the sqrt of the variance of the positions for res1 and res2
+		dist_covar_array[res1,res2] /= sqrt(msd_array[res1]*msd_array[res2])	# Normalizing each matrix element by the sqrt of the variance of the positions for res1 and res2
 		dist_covar_array[res2,res1] = dist_covar_array[res1,res2]
+
+#print 'printing the dist_cavar_array (averaged, mean subtracted, and variance normalized):\n', dist_covar_array[:2,:2]
 
 # OUTPUTING THE DIST COVAR ARRAY
 with open('%03d.%03d.dist_covar_matrix.dat' %(int(sys.argv[3]),end),'w') as f:
 	np.savetxt(f,dist_covar_array)
-
-with open('%03d.%03d.test_covar_matrix.dat' %(int(sys.argv[3]),end),'w') as f:
-	np.savetxt(f,test_array)
 
 # ----------------------------------------
 # FUNCTIONALIZING THE DISTANCE CORRELATION MATRIX (FOR USE IN WISP AND VISUALIZATION)
@@ -150,20 +145,37 @@ else:
 
 # ----------------------------------------
 # CALCULATING THE CARTESIAN COVAR ARRAY OF RESIDUE RESIDUE PAIRS
-for i in range(nSteps):
-	temp_array = flatten(allCoord[i])	# Each element in allCoord has three components (xyz); to get at the xyz components individually, flatten the first index
-	for res1 in range(3*nRes):
-		for res2 in range(res1,3*nRes):
-			cart_covar_array[res1,res2] += temp_array[res1]*temp_array[res2]	# Sum over all timesteps;
-cart_covar_array /= nSteps
+cart_covar_array = zeros((3*nRes, 3*nRes),dtype=np.float64)
+cart_all_array = zeros(3*nRes,dtype=np.float64)
+cart_avg_array = zeros(3*nRes,dtype=np.float64)
+cart_msd_array = zeros(3*nRes,dtype=np.float64)
 
-# COMPLETE THE CARTESIAN COVAR MATRIX ANALYSIS BY SUBTRACTING OUT THE MEAN
-temp_array = flatten(avgCoord)
+cart_avg_array = flatten(avgCoord)
+for i in range(nSteps):
+	cart_all_array = flatten(allCoord[i])	# Each element in allCoord has three components (xyz); to get at the xyz components individually, flatten the timestep index
+	for res1 in range(3*nRes):
+		delta_x = cart_all_array[res1]-cart_avg_array[res1]
+		cart_msd_array[res1] += delta_x*delta_x
+		for res2 in range(res1,3*nRes):
+			temp = cart_all_array[res2]-cart_avg_array[res2]
+			cart_covar_array[res1,res2] += delta_x*temp	# Sum over all timesteps;
+cart_covar_array /= nSteps
+cart_msd_array /= nSteps
+print cart_covar_array[:6,:6]
+print cart_msd_array[:6]
+#
+## COMPLETE THE CARTESIAN COVAR MATRIX ANALYSIS BY SUBTRACTING OUT THE MEAN
 for res1 in range(3*nRes):
 	for res2 in range(res1,3*nRes):
-		cart_covar_array[res1,res2] -= temp_array[res1]*temp_array[res2]	# Subtracting out the mean positions
-		cart_covar_array[res2,res1] = cart_covar_array[res1,res2]			# NOTE: NOT NORMALIZING THE CARTESIAN COVAR MATRIX (BY DIVIDING THE SQRT(VARIANCE) OUT); NOT SURE IF I SHOULD BEFORE PERFORMING A PCA ANALYSIS ON THIS DATASET...
-
+#		if res1 == 0 and res2 == 0:
+#			print sqrt(cart_msd_array[res1]*cart_msd_array[res2])
+		cart_covar_array[res1,res2] /= sqrt(cart_msd_array[res1]*cart_msd_array[res2])
+		cart_covar_array[res2,res1] = cart_covar_array[res1,res2]
+#
+print 'After variance normalization:'
+print cart_covar_array[:6,:6]
+print cart_msd_array[:6]
+sys.exit()
 # OUTPUTING THE CARTESIAN COVAR ARRAY
 with open('%03d.%03d.cart_covar_matrix.dat' %(int(sys.argv[3]),end),'w') as f:
 	np.savetxt(f,cart_covar_array)
