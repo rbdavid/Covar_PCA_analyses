9b44a99591a52f1d461196d5d75f625360fab823
Russell Bruce Davidson <RBD@Russells-MacBook-Pro.local>
2016-08-08 18:26:16 -0600

    fixed ts.frame indexing bug and cleaned up the script

--- Covar_calc.py
+++ Covar_calc.py
@@ -24,7 +24,7 @@ pdb_file = sys.argv[1]
 traj_loc = sys.argv[2]
 start = int(sys.argv[3])
 end = int(sys.argv[4])
-avg_pdb = sys.argv[5]
+avg_pdb = sys.argv[5]		# Used to align frames to; not to be used average positions due to precision error of positions in PDB format
 
 alignment = 'protein and name CA and (resid 20:25 or resid 50:55 or resid 73:75 or resid 90:94 or resid 112:116 or resid 142:147 or resid 165:169 or resid 190:194 or resid 214:218 or resid 236:240 or resid 253:258 or resid 303:307)'
 important = 'protein'
@@ -33,6 +33,7 @@ important = 'protein'
 Functionalize = True
 PCA = True
 
+flatten = np.ndarray.flatten
 zeros = np.zeros
 dot_prod = np.dot
 sqrt = np.sqrt
@@ -58,12 +59,7 @@ avg_important = avg.select_atoms(important)
 
 avg_all.translate(-avg_align.center_of_mass())
 pos0 = avg_align.positions
-
 nRes = avg_important.n_residues
-avgCoord = zeros((nRes,3),dtype=np.float64)
-
-for i in range(nRes):
-	avgCoord[i,:] = avg_important.residues[i].center_of_mass()
 
 # ----------------------------------------
 # INITIATE AND CREATE THE IMPORTANT ATOM SELECTIONS FOR THE IMPORTANT UNIVERSE
@@ -72,37 +68,42 @@ u_all = u.select_atoms('all')
 u_align = u.select_atoms(alignment)
 u_important = u.select_atoms(important)
 
-if nRes != len(u_important.residues):
+if nRes != u_important.n_residues:
 	ffprint('Number of residues do not match between average structure and trajectory universes')
 	sys.exit()
 
 # COLLECTING NSTEPS DATA... NEED TO ITERATE THROUGH ALL TRAJECTORIES TO COLLECT THIS...
 nSteps = 0
-temp = start
-while temp <= end:
-	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,temp,temp))
+while start <= end:
+	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,start,start))
 	nSteps += len(u.trajectory)
-	temp += 1
+	start += 1
+
+start = int(sys.argv[3])
 
 # ARRAY DECLARATION
 allCoord = zeros((nSteps,nRes,3),dtype=np.float64)
-cart_covar_array = zeros((3*nRes, 3*nRes),dtype=np.float64)
+avgCoord = zeros((nRes,3),dtype=np.float64)
 dist_covar_array = zeros((nRes,nRes),dtype=np.float64)
-temp_array = zeros(3*nRes,dtype=np.float64)
 msd_array = zeros(nRes,dtype=np.float64)
 
 # TRAJECTORY ANALYSIS; COLLECTING THE COM INFO OF EACH RESIDUE OF INTEREST
+ffprint('Beginning trajectory analysis')
+temp = 0
 while start <= end:
 	ffprint('Loading trajectory %s' %(start))
 	u.load_new('%sproduction.%s/production.%s.dcd' %(traj_loc,start,start))
-
 	for ts in u.trajectory:
 		u_all.translate(-u_align.center_of_mass())
 		R,d = rotation_matrix(u_align.positions,pos0)
 		u_all.rotate(R)
 		for i in range(nRes):
-			allCoord[ts.frame,i,:] = u_important.residues[i].center_of_mass()
+			allCoord[temp,i,:] = u_important.residues[i].center_of_mass()		# BUG IN ts.frame; will re-zero at the start of the next trajectory...
+			avgCoord[i,:] += u_important.residues[i].center_of_mass()
+		temp += 1
 	start += 1
+
+avgCoord /= nSteps
 
 # ----------------------------------------
 # CALCULATING THE DISTANCE COVAR MATRIX OF RESIDUE RESIDUE PAIRS
@@ -111,14 +112,14 @@ for i in range(nSteps):
 		delta_r = allCoord[i,res1,:] - avgCoord[res1,:]		# Calculating the delta r for every timestep
 		msd_array[res1] += dot_prod(delta_r,delta_r)		# Sum over all timesteps; 
 		for res2 in range(res1,nRes):
-			dist_covar_array[res1,res2] += dot_prod(allCoord[i,res1,:],allCoord[i,res2,:])	# Sum over all timesteps;
+			temp = allCoord[i,res2,:] - avgCoord[res2,:]
+			dist_covar_array[res1,res2] += dot_prod(delta_r,temp)
 dist_covar_array /= nSteps
 msd_array /= nSteps
 
 # COMPLETE THE DISTANCE COVAR MATRIX ANALYSIS BY SUBTRACTING OUT THE MEAN AND NORMALIZING BY THE VARIANCE
 for res1 in range(nRes):
 	for res2 in range(res1,nRes):
-		dist_covar_array[res1,res2] -= dot_prod(avgCoord[res1,:],avgCoord[res2,:])	# Subtracting out the mean positions
 		dist_covar_array[res1,res2] /= sqrt(msd_array[res1]*msd_array[res2])	# Normalizing each matrix element by the sqrt of the variance of the positions for res1 and res2
 		dist_covar_array[res2,res1] = dist_covar_array[res1,res2]
 
@@ -142,19 +143,28 @@ else:
 
 # ----------------------------------------
 # CALCULATING THE CARTESIAN COVAR ARRAY OF RESIDUE RESIDUE PAIRS
+cart_covar_array = zeros((3*nRes, 3*nRes),dtype=np.float64)
+cart_all_array = zeros(3*nRes,dtype=np.float64)
+cart_avg_array = zeros(3*nRes,dtype=np.float64)
+cart_msd_array = zeros(3*nRes,dtype=np.float64)
+
+cart_avg_array = flatten(avgCoord)
 for i in range(nSteps):
-	temp_array = flatten(allCoord[i])	# Each element in allCoord has three components (xyz); to get at the xyz components individually, flatten the first index
+	 cart_all_array = flatten(allCoord[i])	# Each element in allCoord has three components (xyz); to get at the xyz components individually, flatten the first index
 	for res1 in range(3*nRes):
+		delta_x = cart_all_array[res1] - cart_avg_array[res1]
+		cart_msd_array += delta_x*delta_x
 		for res2 in range(res1,3*nRes):
-			cart_covar_array[res1,res2] += temp_array[res1]*temp_array[res2]	# Sum over all timesteps;
+			temp = cart_all_array[res2] - cart_avg_array[res2]
+			cart_covar_array[res1,res2] += delta_x*temp
 cart_covar_array /= nSteps
+cart_msd_array /= nSteps
 
 # COMPLETE THE CARTESIAN COVAR MATRIX ANALYSIS BY SUBTRACTING OUT THE MEAN
-temp_array = flatten(avgCoord)
 for res1 in range(3*nRes):
 	for res2 in range(res1,3*nRes):
-		covar_array[res1,res2] -= temp_array[res1]*temp_array[res2]	# Subtracting out the mean positions
-		covar_array[res2,res1] = covar_array[res1,res2]			# NOTE: NOT NORMALIZING THE CARTESIAN COVAR MATRIX (BY DIVIDING THE SQRT(VARIANCE) OUT); NOT SURE IF I SHOULD BEFORE PERFORMING A PCA ANALYSIS ON THIS DATASET...
+		covar_array[res1,res2] /= sqrt(cart_msd_array[res1]*cart_msd_array[res2])
+		covar_array[res2,res1] = covar_array[res1,res2]	
 
 # OUTPUTING THE CARTESIAN COVAR ARRAY
 with open('%03d.%03d.cart_cover_matrix.dat' %(sys.argv[3],end),'w') as f:
